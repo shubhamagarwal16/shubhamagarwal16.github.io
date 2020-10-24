f3a88a6d37cb06f3165de87a9041d0cb60941784
Shubham Agarwal <shubhamagarwal1694@gmail.com>
2020-10-24 10:54:26 +0000

    CircleCI deploy

---
+++ precache-manifest.5db91eba6a9eb9bfd0ce773294a74ab3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f49edb90c26dbc57c3584fcd144760f8",
+    "url": "/0dc568d46e59e55786cc.worker.js"
+  },
+  {
+    "revision": "69cd844e5198ac97a0aad1207ced67af",
+    "url": "/0dc568d46e59e55786cc.worker.js.LICENSE.txt"
+  },
+  {
+    "revision": "323fa859c80748e2df8394a3c9dd3090",
+    "url": "/index.html"
+  },
+  {
+    "revision": "172a12916bdecc68b940",
+    "url": "/static/css/main.63186839.chunk.css"
+  },
+  {
+    "revision": "967aca476363729647ab",
+    "url": "/static/js/2.a8ce9374.chunk.js"
+  },
+  {
+    "revision": "6c8a6527e5acec6dae71c7cd48f22265",
+    "url": "/static/js/2.a8ce9374.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "172a12916bdecc68b940",
+    "url": "/static/js/main.dd6901d1.chunk.js"
+  },
+  {
+    "revision": "6cd2ea1056b03af6b356",
+    "url": "/static/js/runtime-main.538a3dd4.js"
+  },
+  {
+    "revision": "b3f0d10f04b5343fbe2b82ea66d288d6",
+    "url": "/static/media/GalaxyStore_English.b3f0d10f.png"
+  },
+  {
+    "revision": "71926df275f10a9063d8879e32ef8727",
+    "url": "/static/media/Logo_SA.71926df2.png"
+  },
+  {
+    "revision": "18c8dcb8c7d50975aa235960c99a71d6",
+    "url": "/static/media/Shubham_Agarwal.18c8dcb8.pdf"
+  },
+  {
+    "revision": "c20f3c669eb30dbc3abd73260bb2ffc8",
+    "url": "/static/media/avatar.c20f3c66.jpg"
+  },
+  {
+    "revision": "db9b21a1c41f3dcd9731e1e7acfdbb57",
+    "url": "/static/media/google-play-badge.db9b21a1.png"
+  }
+]);
